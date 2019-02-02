4e55f5cde5b6dc1020284d36389dd999885a289d
Hackintosh 5 <penn.mackintosh@gmail.com>
2019-02-02 16:55:10 +0000

    Typos and comments, and add code to take picture on startup

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import os
-dirname = os.path.dirname(os.realpath(__file__))
+import os, atexit
+dirname = os.path.dirname(os.path.realpath(__file__))
 
+# Initialize the project
 def init():
     initLog()
-    logDebug(
     initCamera()
     initProcessor()
 
+# Load camera and start the preview
 def initCamera():
     picn = 1
     from picamera import PiCamera
@@ -14,38 +15,49 @@ def initCamera():
     camera.resolution = (1296,972)
     camera.start_preview()
 
+# Prepare the logger and add handler to close file safely on exit
 def initLog():
     logn = 1
-    logfile = open(os.path.join(dirname, 'data{:02d}'.format(logn), 'wb')
+    logfile = open(os.path.join(dirname, 'data{:02d}'.format(logn)), 'wb')
     atexit.register(closeLog)
 
+# Import graphics processing libs
 def initProcessor():
     import numpy as np
     import matplotlib.pyplot as plt
     from scipy import ndimage as ndi
     from skimage import feature, io
 
+# Close the log file
 def closeLog():
-    logfile.close()
+    try:
+        logfile.write('S') # End of log
+        logfile.close()
+    except:
+        pass # We don't care.
 
+# This escapes `\`s and `,`s so we can find the end of csv fields
 def sanitizeForLog(data):
     return data.replace('\\', '\\\\').replace(',', '\\,')
 
+# Store a picture to the log
 def storeData(data, location):
     if f.tell() + len(data) > 1000000*35:
         if logn < 5:
             logn += 1
             closeLog()
-            logfile = open(os.path.join(dirname, 'data{:02d}'.format(logn), 'wb')
+            logfile = open(os.path.join(dirname, 'data{:02d}'.format(logn)), 'wb')
         else:
             raise RuntimeException("Out of storage!!!")
     logfile.write('M'+sanitizeForLog(data)+',L'+sanitizeForLog(location)+',')
 
+# Take a photo and store it to the disk, returning the absolute path
 def takePhoto():
     camera.capture(dir_path+"/image_{:03d}.jpg".format(picn))
-    processPhoto(dir_path+"/image_{:03d}.jpg".format(picn))
     picn += 1
+    return dir_path+"/image_{:03d}.jpg".format(picn-1)
 
+# Process the photo, currently just an example. It finds the edges in the photo and draws them onto a plot
 def processPhoto(path):
     im = io.imread(path)
     im = ndi.rotate(im, 15, mode='constant')
@@ -76,15 +88,17 @@ def processPhoto(path):
 
     plt.show()
 
-
+# See how much storage our program is using
 def checkStorage():
     return sum(os.path.getsize(f) for f in os.listdir(dirname) if os.path.isfile(f))
 
+# Cleanup storage if we are over 2G
 def cleanStorage():
     s = checkStorage()
     if s > 1000000000*2:
-        cleanEveryOtherPhoto():
+        cleanEveryOtherPhoto()
 
+# Removes every other photo from the disk.
 def cleanEveryOtherPhoto():
     x = 0
     for f in os.listdir(dirname):
@@ -97,3 +111,6 @@ def cleanEveryOtherPhoto():
 
 if __name__ == "__main__":
     init()
+    x = takePhoto()
+    print(x)
+    processPhoto(x)
